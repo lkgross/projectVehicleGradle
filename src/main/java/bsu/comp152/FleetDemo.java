8d8452dd033cd0b482c6afde43b629765a23aef4
lkgross <profgross@gmail.com>
2023-03-29 12:18:39 -0400

    Trim FleetDemo

--- src/main/java/bsu/comp152/FleetDemo.java
+++ src/main/java/bsu/comp152/FleetDemo.java
@@ -86,10 +86,6 @@ public class FleetDemo {
 
         // TODO: (1) Answer why doesn't this work:
         // System.out.println(fleet[2].getNumAxles());
-        /* This doesn't work because the COMPILER finds that the class (Vehicle)
-         * for the DECLARED type (Vehicle)
-         * of the object (fleet[2]) does NOT define the method (getNumAxles).
-         */
 
         /*
          * We can use CASTING to create a reference with the necessary DECLARED type.
@@ -101,59 +97,24 @@ public class FleetDemo {
         System.out.println("Number of axles on " + truck1.toString() + ":");
         System.out.println(truck1.getNumAxles());
         System.out.println();
-        /*
-        For truck1.getNumAxles(), the COMPILER found that the class (Truck) for the DECLARED type
-        (Truck) of the object truck1 does define the method (getNumAxles).
-        The JVM calls the method that is most appropriate for the ACTUAL type (TractorTrailer)
-        of the object.
 
-        For truck1.toString(), the COMPILER found that the class (Truck) for the DECLARED type
-        (Truck) of the object truck1 does define the method (toString).
-        The JVM calls the method that is most appropriate for the ACTUAL type (TractorTrailer)
-        of the object.
-        */
         // TODO: (3) Determine whether this code will work. Explain the code performance.
         TractorTrailer big = (TractorTrailer) fleet[2];
         System.out.println(big.getNumAxles());
-        // The declared type of big is TractorTrailer.
-        // The compiler confirmed that the TractorTrailer class defines a getNumAxles method.
-        // The actual type of big is TractorTrailer.
-        // The JVM calls the getNumAxles method from the TractorTrailer class.
-        /*
-         * The Object class is a superclass of all other classes.
-         * Thus, we can use an Object variable to store a reference to any object.
-         */
-        Object obj1;
-        obj1 = new Taxi("Ford", "Tempo", 2000, 4, false, false, "T47783");
+
         // TODO: (4) Determine what is the declared type of obj1?  What is its actual type?
         //  Which toString method will be called? Why?
+        Object obj1;
+        obj1 = new Taxi("Ford", "Tempo", 2000, 4, false, false, "T47783");
         System.out.println(obj1.toString());
-        // The declared type of obj1 is Object.
-        // The COMPILER confirms that the class for the DECLARED type of the object (Object)
-        // defines the toString method.
-        // The actual type of obj1 is Taxi.
-        // The JVM calls the method that is most appropriate for the ACTUAL type (Taxi) of the
-        // object.  It calls the toString method from the Taxi class.
 
+        // TODO: (5) Determine the declared and actual types of t1 and t2.
+        //  The methods are executed from which classes when the following code is executed? Explain the reasons.
         Truck t1;
         t1 = new Truck("Mack", "Pinnacle Day Cab", 2016, 10);
         Truck t2;
-        t2 = new TractorTrailer("Mack", "Pinnacle Day Cab", 2016, 10);
-        // TODO: (5) Determine the declared and actual types of t1 and t2.
-        //  The methods are executed from which classes when the following code is executed? Explain the reasons.
+        t2 = new TractorTrailer("Mack", "Pinnacle Day Cab", 2016,10);
         System.out.println(t1.getNumAxles());
-        // The declared type of t1 is Truck.
-        // The COMPILER confirms that the class for the DECLARED type of the object (Truck)
-        // defines the getNumAxles method.
-        // The actual type of t1 is Truck.
-        // The JVM calls the method that is most appropriate for the ACTUAL type (Truck) of the
-        // object.  It calls the getNumAxles method from the Truck class.
-        // The declared type of t2 is Truck.
-        // The COMPILER confirms that the class for the DECLARED type of the object (Truck)
-        // defines the getNumAxles method.
-        // The actual type of t2 is TractorTrailer.
-        // The JVM calls the method that is most appropriate for the ACTUAL type (TractorTrailer) of the
-        // object.  It calls the getNumAxles method from the TractorTrailer class.
         System.out.println(t2.getNumAxles());
     }
 
